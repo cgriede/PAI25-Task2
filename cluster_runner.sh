c035346f2c646583b139f953c0221c78244e1e84
Grieder Cédric <cgriede@ethz.ch>
2025-11-10 00:47:23 +0000

    time 4h

--- cluster_runner.sh
+++ cluster_runner.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 conda activate PAI2
-sbatch -n 8 --mem-per-cpu=2048 --wrap="python -u checker_client.py --results-dir ."
+sbatch -n 8 --mem-per-cpu=2048 --time=4:00:00 --wrap="python -u checker_client.py --results-dir ."
